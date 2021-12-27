677f202aafc8330bbe97c22ceff2dbb0c1ee3ad4
Garrett Jones <garrettcj2010@hotmail.com>
2021-12-27 11:08:59 -0600

    Updated setup.sh, added warnings new logic, and rm commands.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+echo "Make sure you're running this as the correct user you want to install the files on.  Note: If you run this with sudo, it will replace root User files."
+echo "To update current user files just run: ./setup.sh"
+
+echo "Updating files for $(whoami).  Continue?(y/n)"
+read answer
+
+#If answer y or Y then continue else stop
+[ "${answer}" == 'y' ] || [ "${answer}" == 'Y' ] && echo "Replacing files in home dir for user $(whoami) . . ." || echo "Quiting . . ." ; exit
+
 rm ~/.bash_aliases
 rm ~/.bashrc
 rm ~/.profile
@@ -7,3 +16,6 @@ ln -rs bash_aliases ~/.bash_aliases
 ln -rs bashrc ~/.bashrc
 ln -rs profile ~/.profile
 ln -rs vimrc ~/.vimrc
+
+
+echo 'Finished.'
