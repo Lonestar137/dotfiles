8286a7e71ba01dbf794e243e162b48e0b2d60dda
Garrett Jones <garretcj2010@hotmail.com>
2025-10-24 17:53:35 -0500

    Add jpgify.py

---
+++ scripts/jpgify.py
@@ -0,0 +1,48 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [ "pillow==12.0.0" ]
+# ///
+
+# dependencies = [ "pillow==9.0.0" ]
+# To use -
+# cd to directory with pngs and then run:
+# uv run jpgify.sh
+
+import os
+import glob
+import shutil
+import time
+from PIL import Image
+
+def convert_images():
+    # Create a temporary directory
+    folder = f"/tmp/{os.path.basename(os.getcwd())}"
+    os.makedirs(folder, exist_ok=True)
+
+    # Get the list of PNG files
+    png_files = glob.glob("*.png")
+    if not png_files:
+        print("No PNG files found.")
+        return
+
+    # TODO: make this multithreaded
+    # Process each field
+    for original_file in png_files:
+        try:
+            # Create a timestamp prefix
+            timestamp = time.strftime("%Y%m%d_%H%M%S")
+            # Prepare the new filename
+            new_filename = f"{timestamp}_{os.path.splitext(original_file)[0]}.jpg"
+            # Convert PNG to JPG
+            with Image.open(original_file) as img:
+                img.convert("RGB").save(new_filename, "JPEG")
+            # Move hte original file to the temporary directory
+            shutil.move(original_file, folder)
+            print(f"Converted and moved: {original_file} to {new_filename} and {folder}")
+        except Exception as e:
+            print(f"Failed to process {original_file}: {e}")
+            continue
+
+if __name__ == "__main__":
+    convert_images()
+
